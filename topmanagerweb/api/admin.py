a3b34d8523fa0e2b1e599619e9a648e646eeefe5
David Casas Molina <david.casasmolina@gmail.com>
2014-08-22 00:54:55 +0200

    #8 #14 added admin configuration

--- topmanagerweb/api/admin.py
+++ topmanagerweb/api/admin.py
@@ -2,5 +2,15 @@
 # api/admin.py
 
 from django.contrib import admin
+from .models import Country, Position, League, Club, Injury, Footballer, \
+    Nationality, PlayingPosition
 
-# Register your models here.
+admin.site.register(Country)
+admin.site.register(Position)
+admin.site.register(League)
+admin.site.register(Club)
+admin.site.register(Footballer)
+admin.site.register(Injury)
+admin.site.register(Nationality)
+admin.site.register(PlayingPosition)
+
